4569fb0eb2206ec948320a4b6603d86442012e4e
Milo-D <David.Milosevic@web.de>
2019-12-09 21:52:27 +0100

    bash

--- build.sh
+++ build.sh
@@ -3,4 +3,17 @@
 #Currently there is only Support for ATmega32/ATmega32L
 #so please do not change the 'ATMEGA32' Macro.
 
+echo "Compiling MDX..."
+echo -ne '#####                     (33%)\r'
+
 g++ -D ATMEGA32 -I ./Header/ -g -o mdx ./Source/*.cpp
+
+echo -ne '#############             (66%)\r'
+
+sleep 0.2
+
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+echo "Done!"
+
+exit 0
