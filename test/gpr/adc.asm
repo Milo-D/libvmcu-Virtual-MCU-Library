026c679197ec66bcc1112f43d50fa88c4f7dd6cb
Milo-D <David.Milosevic@web.de>
2020-01-02 00:00:20 +0100

    ADC Assembly Test Enhancement

--- test/gpr/adc.asm
+++ test/gpr/adc.asm
@@ -1,20 +1,37 @@
 .INCLUDE "m32def.inc"
 
 ldi r16, 0x00
-ldi r17, 0xff
-sec
+ldi r17, 0x00
+ldi r18, 0x00
 rjmp stack_init
 
 stack_init:
-ldi r18, LOW(RAMEND)
-out SPL, r18
-ldi r18, HIGH(RAMEND)
-out SPH, r18
-rjmp main
+ldi r19, LOW(RAMEND)
+out SPL, r19
+ldi r19, HIGH(RAMEND)
+out SPH, r19
+rjmp outer
 
-main:
-adc r16, r17
-rjmp exit
+outer:
+rcall inner
+cpi r16, 0xff
+breq exit
+rjmp inc_outer
+
+inner:
+mov r18, r16
+adc r18, r17
+cpi r17, 0xff 
+brne inc_inner
+ret
+
+inc_outer:
+inc r16
+rjmp outer
+
+inc_inner:
+inc r17
+rjmp inner
 
 exit:
 nop
