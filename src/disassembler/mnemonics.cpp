2924a75a46a706e5c20f85bba402e81b7724f1bf
Milo-D <David.Milosevic@web.de>
2020-01-02 01:16:35 +0100

    Added: CLH Instruction

--- src/disassembler/mnemonics.cpp
+++ src/disassembler/mnemonics.cpp
@@ -800,6 +800,17 @@ string clz(int opcode) {
     return stream.str();
 }
 
+string clh(int opcode) {
+
+    stringstream stream;
+
+    stream << "clh";
+    stream << fill(stream.str().size());
+    stream << "; HF <- 0x00";
+
+    return stream.str();
+}
+
 string bclr(int opcode) {
 
     int s_bit = extract(opcode, 4, 7, 0);
@@ -816,5 +827,6 @@ string bclr(int opcode) {
 string (*mnemonics[INSTR_MAX]) (int opcode) = { nop, movw, muls, mulsu, fmul, ldi, rjmp, mov, 
                                                 dec, inc, add, adc, sub, sbc, push, pop, out, clr, ld_x, ld_xi, ld_dx, ld_y, ld_z, 
                                                 st_x, st_xi, brne, breq, brge, brpl, brlo, rcall, ret, cp, cpi, lsr, ori, or_asm, and_asm, 
-                                                andi, com, bld, bst, ses, set, sev, sez, seh, sec, sei, sen, cls, clt, clv, clz, bclr };
+                                                andi, com, bld, bst, ses, set, sev, sez, seh, sec, sei, sen, cls, clt, clv, clz, clh, 
+                                                bclr };
 
--- src/instructions/instructions.cpp
+++ src/instructions/instructions.cpp
@@ -807,6 +807,11 @@ void clz(Sys *sys, int opcode) {
     sys->write_sreg(ZF, 0x00);
 }
 
+void clh(Sys *sys, int opcode) {
+
+    sys->write_sreg(HF, 0x00);
+}
+
 void bclr(Sys *sys, int opcode) {
 
     int s_bit = extract(opcode, 4, 7, 0);
@@ -817,7 +822,8 @@ void bclr(Sys *sys, int opcode) {
 void (*instructions[INSTR_MAX]) (Sys *sys, int opcode) = { nop, movw, muls, mulsu, fmul, ldi, rjmp, mov, 
                                                            dec, inc, add, adc, sub, sbc, push, pop, out, clr, ld_x, ld_xi, ld_dx, ld_y, ld_z, 
                                                            st_x, st_xi, brne, breq, brge, brpl, brlo, rcall, ret, cp, cpi, lsr, ori, or_asm, and_asm, 
-                                                           andi, com, bld, bst, ses, set, sev, sez, seh, sec, sei, sen, cls, clt, clv, clz, bclr };
+                                                           andi, com, bld, bst, ses, set, sev, sez, seh, sec, sei, sen, cls, clt, clv, clz, clh, 
+                                                           bclr };
 
 
 
