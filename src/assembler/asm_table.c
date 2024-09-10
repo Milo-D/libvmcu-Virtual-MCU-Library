e0384fa422b7d2350ebde7f0eb8d620cf37a8b31
Milo-D <milod2048@gmail.com>
2024-09-10 13:19:35 +0200

    assembler: add mulsu instruction

--- src/assembler/asm_table.c
+++ src/assembler/asm_table.c
@@ -33,7 +33,13 @@ static asm_table_entry_t asm_table[VMCU_IID_ENUM_END] = {
         .n_operands       = 2,
         .n_words          = 1
     },
-    [VMCU_IID_MULSU]   = { .handler = assemble_nop, .handler_unpacked = pack_and_assemble_nop, .n_operands = 2, .n_words = 1 },
+    [VMCU_IID_MULSU]   = {
+
+        .handler          = assemble_mulsu,
+        .handler_unpacked = pack_and_assemble_mulsu,
+        .n_operands       = 2,
+        .n_words          = 1
+    },
     [VMCU_IID_FMUL]    = { .handler = assemble_nop, .handler_unpacked = pack_and_assemble_nop, .n_operands = 2, .n_words = 1 },
     [VMCU_IID_FMULS]   = { .handler = assemble_nop, .handler_unpacked = pack_and_assemble_nop, .n_operands = 2, .n_words = 1 },
     [VMCU_IID_FMULSU]  = { .handler = assemble_nop, .handler_unpacked = pack_and_assemble_nop, .n_operands = 2, .n_words = 1 },
--- src/assembler/handlers.h
+++ src/assembler/handlers.h
@@ -15,5 +15,7 @@ vmcu_rc_t pack_and_assemble_mul(uint16_t* write_ptr, const va_list operands);
 vmcu_rc_t assemble_mul(uint16_t* write_ptr, const vmcu_operand_t* dest, const vmcu_operand_t* src);
 vmcu_rc_t pack_and_assemble_muls(uint16_t* write_ptr, const va_list operands);
 vmcu_rc_t assemble_muls(uint16_t* write_ptr, const vmcu_operand_t* dest, const vmcu_operand_t* src);
+vmcu_rc_t pack_and_assemble_mulsu(uint16_t* write_ptr, const va_list operands);
+vmcu_rc_t assemble_mulsu(uint16_t* write_ptr, const vmcu_operand_t* dest, const vmcu_operand_t* src);
 
 #endif
